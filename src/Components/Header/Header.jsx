8ffea55fa07a7d508fb398bab5d5b8ba64c0c7d5
Luiz Bonini <lc.bonini13@gmail.com>
2022-12-27 12:39:03 -0300

    Feat: Header user name

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -2,19 +2,22 @@ import React from 'react'
 import { useNavigate } from 'react-router-dom'
 
 import {ReactComponent as HeaderImg} from '../../Assets/Imgs/pokedexHeader.svg'
+import User from '../../Assets/Imgs/User.svg'
 import pokeicon from '../../Assets/Imgs/Pokeicon.svg'
 import styles from './Header.module.css'
 
 const Header = () => {
 
   const navigate = useNavigate()
+  const nome = window.localStorage.getItem('nome')
 
   return (
     <div className={styles.header}>
+        <HeaderImg className={styles.headerImg}/>
+        <p className={styles.user}><img src={User} alt="" className={styles.userIcon}/>{nome}</p>
         <button className={styles.gotchas} onClick={() => navigate('/login')}>
           <img src={pokeicon} alt="Pokeball icon" className={styles.pokeicon}/>
         </button>
-        <HeaderImg className={styles.headerImg}/>
     </div>
   )
 }
